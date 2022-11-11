ae842d9f4118b8179ffa1e4eb6dfdce58595d172
abilov.w <109854878+Shirxan@users.noreply.github.com>
2022-11-11 19:38:24 +0400

    Add files via upload

---
+++ 11.11/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HeadersVue from './components/Headers.vue';
+import SectionVue from './components/Section.vue';
+</script>
+<template>
+  <div class="container">
+    <HeadersVue/>
+    <SectionVue/>
+  </div>
+</template>
---
+++ 11.11/src/components/Section.vue
@@ -0,0 +1,343 @@
+<template>
+    <div>
+        <section id="ic_one">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <div id="trade">
+                            <img src="" alt="">
+                        </div>
+                        <div class="row d-flex">
+                            <div class="col-6">
+                                <div id="tradesmall">
+                                    <img src="" alt="">
+                                </div>
+
+                            </div>
+                            <div class="col-6">
+                                <div id="tradesmall1">
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-6">
+                        <h2>NFT Killer - 2.0</h2>
+                        <div class="row">
+                            <div class="col-2">
+                               <div id="ss">
+                                <i class="fa-brands fa-ethereum" ></i>
+                               </div>
+                            </div>
+                            <div class="col-4">
+                                <small>Current Bind</small>
+                                <h3>2.39 ETH</h3>
+                            </div>
+
+                            <div class="col-6">
+                                <small>59 Stock</small>
+                            </div>
+                            <p>Proin massa dui, maximus vitae massa in, ullamcorper euismod justo. Ut condimentum ipsum
+                                id nibh suscipit, eget iaculis mi mollis. Proin quis turpis odio. Suspendisse non ex a
+                                leo lobortis tincidunt condimentum quis sem. Sed ornare nunc vel mi eleifend, a posuere
+                                mauris efficitur. Duis sed velit est.</p>
+                            <div class="row">
+                             
+                                <button id="pageBid">Place bid</button>
+                                
+                            </div>
+                            <div class="page mt-5 ">
+                                <ul type="none">
+                                    <li>
+                                        <a href="#">Bid History</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Info</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Details</a>
+                                    </li>
+                                </ul>
+                            </div>
+                         
+                            <div id="divs">
+                                <div class="row">
+                                    <div class="col-2 mt-1 offset-1">
+                                        <div class="yumru">
+                                            <img src="" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <span>By <small>Creativemela</small> <br> Place a bid</span> <strong>@ 3d Models</strong>
+
+                                    </div>
+                        
+                                    <div class="col-3 d-flex ">
+                                        <i class="fa-brands fa-ethereum" ></i>
+                                        <div class="change offset-1">
+                                            <h3>2.39 ETH</h3>
+                                            <span>=$10.56</span>
+                                        </div>
+                                       
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-2 mt-1 offset-1">
+                                        <div class="yumru">
+                                            <img src="" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <span>By <small>Creativemela</small> <br> Place a bid</span> <strong>@ 3d Models</strong>
+
+                                    </div>
+                        
+                                    <div class="col-3 d-flex ">
+                                        <i class="fa-brands fa-ethereum" ></i>
+                                        <div class="change offset-1">
+                                            <h3>2.39 ETH</h3>
+                                            <span>=$10.56</span>
+                                        </div>
+                                       
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-2 mt-1 offset-1">
+                                        <div class="yumru">
+                                            <img src="" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <span>By <small>Creativemela</small> <br> Place a bid</span> <strong>@ 3d Models</strong>
+
+                                    </div>
+                        
+                                    <div class="col-3 d-flex ">
+                                        <i class="fa-brands fa-ethereum" ></i>
+                                        <div class="change offset-1">
+                                            <h3>2.39 ETH</h3>
+                                            <span>=$10.56</span>
+                                        </div>
+                                       
+                                    </div>
+                                </div>
+                                <hr>
+                            </div>
+
+
+
+
+
+
+
+                            <!-- Buttons Div -->
+                            <div class="buttonIcon">
+                                <div class="face">
+                                    <button class="iconsClass face ">
+                                        <a href="">
+                                            <i class="fa-brands fa-facebook"></i>
+                                            Facebook
+                                        </a>
+                                    </button>
+                                </div>
+                                <div class="google">
+                                    <button class="iconsClass google">
+                                        <a href="">
+                                            <i class="fa-brands fa-google-plus-g"></i>
+                                            Google
+                                        </a>
+                                    </button>
+                                </div>
+                                <div class="twitter">
+                                    <button class="iconsClass  twitter">
+                                        <a href="">
+                                            <i class="fa-brands fa-twitter"></i>
+                                            Twitter
+                                        </a>
+                                    </button>
+                                </div>
+                                <div class="instagram">
+                                    <button class="iconsClass instagram">
+                                        <a href="">
+                                            <i class="fa-brands fa-instagram"></i>
+                                            Instagram
+                                        </a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </div>
+</template>
+<style scoped>
+header {
+    background-image: url(img/img.png);
+    background-position: center;
+    background-repeat: no-repeat;
+
+    background-color: #292227;
+    background-size: cover;
+
+
+}
+
+#ic_one {
+    background-color: #241b20;
+}
+ul{
+    display: flex  !important;
+    justify-content: space-between;
+}
+li a{
+    text-decoration: none;
+    color: #ffff;
+}
+
+h1 {
+
+    font-family: 'Press Start 2P';
+    text-align: center;
+
+    padding-top: 240px;
+
+    color: #ffff;
+
+}
+
+#ic_one h3 {
+    font-family: "Press Start 2P";
+    font-size: 14px;
+    color: #EE46BC;
+
+
+
+}
+
+#ic_one h4 {
+    font-family: 'IBM Plex Mono';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 30px;
+
+
+    text-transform: capitalize;
+
+    color: #EE46BC;
+
+}
+
+#ic_one small {
+    font-family: 'IBM Plex Mono';
+
+    font-size: 13px;
+
+    color: #B09BB6;
+}
+
+#ic_one p {
+    font-family: 'IBM Plex Mono';
+
+    font-size: 14px;
+    line-height: 30px;
+    color: #B09BB6;
+}
+
+#ic_one .col-4 {
+    margin-left: -70px;
+}
+
+#ic_one h2 {
+
+    font-family: 'Press Start 2P';
+
+    color: #FFFFFF
+}
+
+#ss i {
+    font-size: 40px;
+    color: #EE46BC;
+}
+
+#ic_one .container {
+    padding-top: 180px;
+}
+
+
+#ic_first small {
+
+
+    font-size: 18px;
+
+    color: #B09BB6;
+
+    font-family: 'IBM Plex Mono';
+
+
+
+}
+
+#ic_first {
+    width: 180px;
+    margin: auto;
+    padding-bottom: 170px;
+}
+
+#ic_first p {
+
+
+    font-size: 18px;
+
+    color: #ffff;
+    font-family: 'IBM Plex Mono';
+
+
+
+
+}
+
+#divs hr {
+    background: #FFFFFF1A;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    height: 1px;
+}
+
+#divs strong {
+    color: #FFFFFF;
+    font-family: 'IBM Plex Mono';
+    font-weight: 600;
+    font-size: 13px;
+}
+
+#divs small {
+    color: #EE46BC;
+}
+
+#divs span {
+    width: 180px;
+    font-family: 'IBM Plex Mono';
+    font-weight: 600;
+    font-size: 13px;
+    text-transform: capitalize;
+    color: #B09BB6;
+}
+
+#divs i {
+    font-size: 30px;
+    color: #EE46BC;
+}
+
+.yumru {
+    width: 35px;
+    height: 35px;
+    background-color: #3f2339;
+    border-radius: 50%;
+}
+</style>
